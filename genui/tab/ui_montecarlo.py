465ba9b4a12ae791d15035b93219c6c49e813efc
Jiri Vrany <jiri.vrany@tul.cz>
2013-01-24 09:59:04 +0100

    zmena ui pro monte carlo a sensivity

--- genui/tab/ui_montecarlo.py
+++ genui/tab/ui_montecarlo.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'montecarlo.ui'
 #
-# Created: Tue Jan 15 10:33:18 2013
+# Created: Thu Jan 24 09:57:48 2013
 #      by: PyQt4 UI code generator 4.9.6
 #
 # WARNING! All changes made in this file will be lost!
@@ -48,6 +48,12 @@ class Ui_MonteCarlo(object):
         self.edit_monte_sigma = QtGui.QLineEdit(self.groupBox_monte_1)
         self.edit_monte_sigma.setObjectName(_fromUtf8("edit_monte_sigma"))
         self.formLayout_4.setWidget(1, QtGui.QFormLayout.FieldRole, self.edit_monte_sigma)
+        self.label = QtGui.QLabel(self.groupBox_monte_1)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.formLayout_4.setWidget(2, QtGui.QFormLayout.LabelRole, self.label)
+        self.label_2 = QtGui.QLabel(self.groupBox_monte_1)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.formLayout_4.setWidget(3, QtGui.QFormLayout.LabelRole, self.label_2)
         self.groupBox_monte_buttons = QtGui.QGroupBox(self.groupBox_monte_1)
         self.groupBox_monte_buttons.setTitle(_fromUtf8(""))
         self.groupBox_monte_buttons.setObjectName(_fromUtf8("groupBox_monte_buttons"))
@@ -61,21 +67,13 @@ class Ui_MonteCarlo(object):
         self.horizontalLayout_18.addWidget(self.button_monte_save)
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout_18.addItem(spacerItem)
-        self.formLayout_4.setWidget(3, QtGui.QFormLayout.SpanningRole, self.groupBox_monte_buttons)
-        self.groupBox = QtGui.QGroupBox(self.groupBox_monte_1)
-        self.groupBox.setObjectName(_fromUtf8("groupBox"))
-        self.verticalLayout = QtGui.QVBoxLayout(self.groupBox)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.check_cond = QtGui.QCheckBox(self.groupBox)
-        self.check_cond.setObjectName(_fromUtf8("check_cond"))
-        self.verticalLayout.addWidget(self.check_cond)
-        self.check_storat = QtGui.QCheckBox(self.groupBox)
-        self.check_storat.setObjectName(_fromUtf8("check_storat"))
-        self.verticalLayout.addWidget(self.check_storat)
-        self.check_porosity = QtGui.QCheckBox(self.groupBox)
-        self.check_porosity.setObjectName(_fromUtf8("check_porosity"))
-        self.verticalLayout.addWidget(self.check_porosity)
-        self.formLayout_4.setWidget(2, QtGui.QFormLayout.LabelRole, self.groupBox)
+        self.formLayout_4.setWidget(4, QtGui.QFormLayout.SpanningRole, self.groupBox_monte_buttons)
+        self.edit_monte_storativity = QtGui.QLineEdit(self.groupBox_monte_1)
+        self.edit_monte_storativity.setObjectName(_fromUtf8("edit_monte_storativity"))
+        self.formLayout_4.setWidget(2, QtGui.QFormLayout.FieldRole, self.edit_monte_storativity)
+        self.edit_monte_porosity = QtGui.QLineEdit(self.groupBox_monte_1)
+        self.edit_monte_porosity.setObjectName(_fromUtf8("edit_monte_porosity"))
+        self.formLayout_4.setWidget(3, QtGui.QFormLayout.FieldRole, self.edit_monte_porosity)
         self.horizontalLayout.addWidget(self.groupBox_monte_1)
         self.groupBox_monte_2 = QtGui.QGroupBox(MonteCarlo)
         self.groupBox_monte_2.setObjectName(_fromUtf8("groupBox_monte_2"))
@@ -93,12 +91,10 @@ class Ui_MonteCarlo(object):
         MonteCarlo.setWindowTitle(_translate("MonteCarlo", "Form", None))
         self.groupBox_monte_1.setTitle(_translate("MonteCarlo", "Monte Carlo", None))
         self.label_sens_mult_25.setText(_translate("MonteCarlo", "How many tasks?", None))
-        self.label_sens_mult_26.setText(_translate("MonteCarlo", "Sigma", None))
+        self.label_sens_mult_26.setText(_translate("MonteCarlo", "Hydraulic cond. sigma", None))
+        self.label.setText(_translate("MonteCarlo", "Storativity", None))
+        self.label_2.setText(_translate("MonteCarlo", "Dual porosity", None))
         self.button_monte_compute.setText(_translate("MonteCarlo", "Compute", None))
         self.button_monte_save.setText(_translate("MonteCarlo", "Generate tasks", None))
-        self.groupBox.setTitle(_translate("MonteCarlo", "Compute with", None))
-        self.check_cond.setText(_translate("MonteCarlo", "Hydraulic conductivity", None))
-        self.check_storat.setText(_translate("MonteCarlo", "Storativity", None))
-        self.check_porosity.setText(_translate("MonteCarlo", "Dual porosity", None))
         self.groupBox_monte_2.setTitle(_translate("MonteCarlo", "List of materials", None))
 
